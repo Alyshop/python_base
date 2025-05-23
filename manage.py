2b21c19b49d409875d782a3c98e2f035542bffee
Alyshoper <alyssondiego18@gmail.com>
2025-05-23 20:17:47 -0300

    sync repo

---
+++ manage.py
@@ -0,0 +1,10 @@
+"""
+echo "# python_base" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Alyshop/python_base.git
+git push -u origin main
+
+"""
